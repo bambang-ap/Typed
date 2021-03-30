505331f1f250e3a9314e767f463bd2184d4e28a8
Bambang Adhitya <adhyt.scott@gmail.com>
2021-03-30 11:09:15 +0700

    names

--- node-qris/index.d.ts
+++ node-qris/index.d.ts
@@ -1,6 +1,17 @@
 declare module 'node-qris' {
 	import _parse from 'node-qris'
-	type Ret = { name: string, length: number, data: string }
+	type Names =
+		'PayloadFormatIndicator' |
+		'PointOfInitiationMethod' |
+		'MerchantCategoryCode' |
+		'TransactionCurrency' |
+		'TransactionAmount' |
+		'CountryCode' |
+		'MerchantName' |
+		'MerchantCity' |
+		'PostalCode' |
+		'CRC'
+	type Ret = { name: Names, length: number, data: string }
 	function parse(qrCode: string): Ret[]
 	export default parse
 }
