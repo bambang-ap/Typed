d8c32390221d0dabae5d0ff8b896f097ff8a622c
aprakoso98 <adhyt.scott@gmail.com>
2021-12-21 13:23:45 +0700

    moveApp

--- rn-command-dist/src/moveApp.js
+++ rn-command-dist/src/moveApp.js
@@ -4,13 +4,14 @@ var child_process_1 = require("child_process");
 var moment = require("moment");
 var bin_1 = require("../bin");
 var ANDROID_PATH = "android";
-var apkPath = "./" + ANDROID_PATH + "/app/build/outputs/apk/release/app-release.apk";
-var aabPath = "./" + ANDROID_PATH + "/app/build/outputs/bundle/release/app.aab";
 var outputFolder = "outputs";
 function moveApp() {
     var _a;
     var projectName = require(process.env.PWD + "/package.json").name;
-    var _b = bin_1.theParams, _filename = _b["--filename"], __filename = _b["-f"];
+    var _b = bin_1.theParams, _filename = _b["--filename"], __filename = _b["-f"], _source = _b["--source"], __source = _b["-s"];
+    var source = _source !== null && _source !== void 0 ? _source : __source;
+    var apkPath = "./" + ANDROID_PATH + "/app/build/outputs/apk" + (source !== null && source !== void 0 ? source : '/release/app-release.apk');
+    var aabPath = "./" + ANDROID_PATH + "/app/build/outputs/bundle" + (source !== null && source !== void 0 ? source : '/release/app.aab');
     var filename = projectName + "-Bundle-" + moment().format('YYYY-MM-DD-HH-mm-ss') + ".aab";
     var pathFile = apkPath;
     if ('aab' in bin_1.theParams) {
